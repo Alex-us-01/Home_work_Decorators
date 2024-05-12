d8b1192083b2e1e0fcedb313cb54686168017e84
Uskov Aleksandr <alex_us_01@mail.ru>
2024-05-12 21:30:11 +0300

    Change tas_3.py

--- task_3.py
+++ task_3.py
@@ -19,11 +19,9 @@ def gen_headers():
 list_urls = ['yandex.ru', 'mail.ru', 'habr.com', 'stackoverflow.com']
 
 
-# response = requests.get('https://spb.hh.ru/search/vacancy?text=python&area=1&area=2', headers=gen_headers())
-
 @logger(LOGFILE)
-def test_request(url):
-    response = requests.get(f'https://{url}/', headers=gen_headers())
+def test_request(user_url):
+    response = requests.get(f'https://{user_url}/', headers=gen_headers())
     print(response.status_code)
     return f'STATUS CODE - {response.status_code}'
 
